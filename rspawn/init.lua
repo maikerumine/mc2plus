ee89ea78291c146b4e0b4cc56a15b01e65a5f4db
maikerumine <michaellorenger@yahoo.com>
2017-06-22 00:52:46 -0400

    fix spawn for players.

--- rspawn/init.lua
+++ rspawn/init.lua
@@ -1,9 +1,38 @@
-local origin = minetest.setting_get_pos("static_spawnpoint") or {x=0, y=-30, z=0}
+minetest.register_chatcommand("killme", {
+	description = "Kill yourself to respawn",
+	func = function(name)
+		local player = minetest.get_player_by_name(name)
+		if player then
+			if minetest.settings:get_bool("enable_damage") then
+				player:set_hp(0)
+				return true
+			else
+				for _, callback in pairs(core.registered_on_respawnplayers) do
+					if callback(player) then
+						return true
+					end
+				end
+
+				-- There doesn't seem to be a way to get a default spawn pos from the lua API
+				return false, "No static_spawnpoint defined"
+			end
+		else
+			-- Show error message if used when not logged in, eg: from IRC mod
+			return false, "You need to be online to be killed!"
+		end
+	end
+})
+
+
+
+
+local origin = minetest.setting_get_pos("static_spawnpoint") or {x=0, y=40, z=0}
 local adminname = minetest.setting_get("name") or "singleplayer"
 
 local spawnanywhere = minetest.setting_get("spawn_anywhere") or false
 
 local playerspawns = {}
+
 local spawnsfile = minetest.get_worldpath().."/dynamicspawns.lua.ser"
 
 local bedspawn = minetest.setting_getbool("enable_bed_respawn")
@@ -33,11 +62,13 @@ local function newspawn(pos, radius)
 	if not radius then
 		radius = 32
 	end
+
 	if radius > 556 then
 		minetest.log("error", "No valid spawnable location")
 		return
 	end
 
+	
 	--minetest.debug("Trying somewhere around "..minetest.pos_to_string(pos))
 
 	local breadth = radius
@@ -135,6 +166,10 @@ minetest.register_chatcommand("setspawn", {
 })
 ]]
 
+
+
+
+
 --[[
 function spawnsave()
 	local serdata = minetest.serialize(playerspawns)
@@ -162,6 +197,8 @@ end
 
 spawnload()
 ]]
+
+
 minetest.register_on_newplayer(function(player)
 	local name = player:get_player_name()
 	playerspawns[name] = player:getpos()
@@ -171,6 +208,12 @@ minetest.register_on_newplayer(function(player)
 	end
 	return
 end)
+
+
+
+
+--removed to keep beds and not random on reconnect
+
 --[[
 minetest.register_on_joinplayer(function(player)
 	minetest.after(1.1, function()
@@ -178,45 +221,29 @@ minetest.register_on_joinplayer(function(player)
 		if not playerspawns[name] then
 			playerspawns[name] = newspawn()
 			player:setpos(playerspawns[name])
-			--spawnsave()
+			spawnsave()
 		end
 	end)
 end)
 ]]
---[[
 minetest.register_on_respawnplayer(function(player)
 	local name = player:get_player_name()
 	if bedspawn == true then
-		local pos = beds.spawn[name]
+		--local pos = mcl_beds.spawn[name] --bedsglobal
+		local pos = minetest.string_to_pos(player:get_attribute("mcl_beds:spawn"))
 		if pos then
 			player:setpos(pos)
 			return true
 		end
 	end
 	-- And if no bed, nor bed spwawning not active:
-	player:setpos(playerspawns[name])
+	--player:setpos(playerspawns[name])
+	--player:setpos( {x=0, y=35, z=0} ) --works
+	player:setpos({
+				x = math.random(-500,500),
+				y = math.random(10, 50),
+				z = math.random(-500,500),
+				})
 	return true
 end)
-]]
-minetest.register_on_joinplayer(function(player)
-minetest.after(1.1, function()
-	local name = player:get_player_name()
-	playerspawns[name] = player:getpos()
-	--spawnsave()
-	if spawnanywhere then
-		genpos(player:get_player_name() )
-	end
-	return
-	end)
-end)
-minetest.register_on_respawnplayer(function(player)
-minetest.after(1.1, function()
-	local name = player:get_player_name()
-	playerspawns[name] = player:getpos()
-	--spawnsave()
-	if spawnanywhere then
-		genpos(player:get_player_name() )
-	end
-	return
-	end)
-end)
+
